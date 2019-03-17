bc304aec1ff99dff9f7f2fb72719b3f1a8049fa4
vikasbidare <mailme.vikasb@gmail.com>
2019-03-17 22:13:32 +0530

    Commmits!!!

--- app/src/main/java/com/example/android/s4s/CS.java
+++ app/src/main/java/com/example/android/s4s/CS.java
@@ -2,15 +2,20 @@ package com.example.android.s4s;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -72,26 +77,28 @@ public class CS extends Fragment {
         rootref = FirebaseDatabase.getInstance().getReference();
         userref = rootref.child("Seller");
 
-//        userref.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                for(DataSnapshot ds: dataSnapshot.getChildren())
-//                {
-////                    Log.d("fuck",ds.toString());
-//                    String author = ds.child("Author's Name").getValue().toString();
-//                    Log.d("fuck",author.toString());
-//                    String title  = ds.child("Book Name").getValue().toString();
-//                    String price = ds.child("Price").getValue().toString();
-//
-//                    books.add(new Book(title,author,price,"Add",R.drawable.ic_menu_gallery,R.drawable.book_ratings,R.drawable.ic_add_shopping_cart));
-//                }
-//            }
+        userref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot ds: dataSnapshot.getChildren())
+                {
+                    for(DataSnapshot ds1: ds.getChildren()) {
+                        Log.d("fuck", ds.toString());
+                        String author = ds.child("Author's Name").getValue().toString();
+                        Log.d("fuck", author.toString());
+                        String title = ds.child("Book Name").getValue().toString();
+                        String price = ds.child("Price").getValue().toString();
 
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
+                        books.add(new Book(title, author, price, "Add", R.drawable.ic_menu_gallery, R.drawable.book_ratings, R.drawable.ic_add_shopping_cart));
+                    }
+                }
+    }
+
+           @Override
+           public void onCancelled(@NonNull DatabaseError databaseError) {
+
+           }
+      });
 
 
 
